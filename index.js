09a44d34fe953030e87041da62dac867129dbfb7
NarmolMiz <58323347+NarmolMiz@users.noreply.github.com>
2020-01-11 15:36:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+const config = require("./config.json");
+
+ot.on("ready", function () {
+ bot.user.setGame(`Hi, Je suis la pour foutre la merde..`);
+ console.log(`${bot.user.username} is Ready!`);
+});
+
+bot.login(process.env.TOKEN);
